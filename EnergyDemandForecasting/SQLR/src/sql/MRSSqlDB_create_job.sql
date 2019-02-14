00ac4d62884a95c39f7ef7ee00116fd08f7efc26
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-14 13:16:12 -0800

    updating create_job.sql

--- EnergyDemandForecasting/SQLR/src/sql/MRSSqlDB_create_job.sql
+++ EnergyDemandForecasting/SQLR/src/sql/MRSSqlDB_create_job.sql
@@ -2,20 +2,9 @@ USE $(DBName)
 GO
 print '$(DBName)'
 
-print 'Deleting job'
 exec usp_delete_job '$(DBName)'
-GO
-print 'Deleted job'
+
 print '$(ServerName)'
-print '$(Port)'
-declare @server varchar(100)
 
-if (rtrim(ltrim('$(Port)')) <> 'NA')
-	set @server = concat('$(ServerName)',",", '$(Port)')
-else
-	set @server = '$(ServerName)'
-
-exec usp_create_job @server, '$(DBName)', '$(UserName)', '$(Pswd)',  '$(WindowsAuth)'
+exec usp_create_job '$(ServerName)', '$(DBName)', '$(UserName)', '$(Pswd)',  '$(WindowsAuth)'
 GO
-
-
