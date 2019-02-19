594c5088290987c91b7db99ee97a98adbc577971
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-19 09:58:02 -0800

    Adding packages R

---
+++ ProductCrossSell/Resources/packages.R
@@ -0,0 +1,5 @@
+# installing packages required for this solution
+# plyr and zoo required
+install.packages("reshape", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
+install.packages("reshape", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
+print("Done installing packages")
