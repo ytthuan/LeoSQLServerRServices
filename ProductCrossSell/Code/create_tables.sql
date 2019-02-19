fc2463893907677267a4cefb0155b6b54610674c
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-19 10:25:34 -0800

    Updating productscrosssell

---
+++ ProductCrossSell/Code/create_tables.sql
@@ -0,0 +1,32 @@
+/****** Object:  Table [dbo].[ProductXSL]    Script Date: 2/19/2019 6:20:24 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ProductXSL](
+	[cust_ID] [int] NULL,
+	[Womens_Casual] [bit] NULL,
+	[Ties] [bit] NULL,
+	[Mens_Casual] [bit] NULL,
+	[Small_Appliance] [bit] NULL,
+	[Bath] [bit] NULL,
+	[Mens_Sports] [bit] NULL,
+	[Mens_Shoes] [bit] NULL,
+	[Womens_Suits] [bit] NULL,
+	[Kitchen] [bit] NULL,
+	[Linens] [bit] NULL,
+	[Dining] [bit] NULL,
+	[Belts] [bit] NULL,
+	[Dresses] [bit] NULL,
+	[Cosmetics] [bit] NULL,
+	[Womens_Sports] [bit] NULL,
+	[Mens_Suits] [bit] NULL,
+	[Housewares] [bit] NULL,
+	[Womens_Shoes] [bit] NULL,
+	[Bedroom] [bit] NULL
+) ON [PRIMARY]
+GO
+
+
