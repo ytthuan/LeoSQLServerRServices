890bf8ae3dfecf7cea5bb9f8655618ae5b01d057
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-19 13:45:11 -0800

    updating packages.R

--- RetailForecasting/Resources/packages.R
+++ RetailForecasting/Resources/packages.R
@@ -1,3 +1,5 @@
 # installing packages required for this solution
-
+# forecast required
+install.packages("forecast", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
+install.packages("forecast", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
 print("Done installing packages")
