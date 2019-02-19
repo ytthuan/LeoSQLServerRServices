d4d5faacd63afffb550eeadb15605cd0185e7998
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-19 13:15:27 -0800

    updaitng packages.R

--- ProductCrossSell/Resources/packages.R
+++ ProductCrossSell/Resources/packages.R
@@ -1,5 +1,5 @@
 # installing packages required for this solution
-# plyr and zoo required
+# reshape required
 install.packages("reshape", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
 install.packages("reshape", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
 print("Done installing packages")
